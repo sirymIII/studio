198043b07d8da6db56c7bb90de7425437daddadc
Mukhtar Yerima <mukhtar6369@bazeuniversity.edu.ng>
2025-09-25 13:44:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1591778599-3abae",
+  "appId": "1:511672447236:web:0af17d1d465723b8c56051",
+  "apiKey": "AIzaSyCpBTkvvxSqnUT9BeYeWLwiv58cSs3TfS8",
+  "authDomain": "studio-1591778599-3abae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "511672447236"
+};
